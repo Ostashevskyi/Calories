7ce31cb02968362588adc6bff6f636384f13cd01
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-07-30 13:32:01 +0300

    refactoring

--- store/index.js
+++ store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import weatherSlice from "./weatherSlice";
 import caloriesSlice from "./caloriesSlice";
 
 const store = configureStore({
   reducer: {
-    weather: weatherSlice,
     calories: caloriesSlice,
   },
 });

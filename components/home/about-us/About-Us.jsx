90b423d04a64cb3efbde2b77d82f1ffa965140ab
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-07-30 12:48:41 +0300

    change ' to &apos;

--- components/home/about-us/About-Us.jsx
+++ components/home/about-us/About-Us.jsx
@@ -6,20 +6,20 @@ export default function AboutUs() {
       <H2White>About Us</H2White>
       <div>
         <p className="text-center text-white text-[26px] font-normal px-16 font-serif pb-[200px]">
-          At CalorieTrack, we're passionate about empowering you to achieve your
-          health and fitness goals through mindful nutrition. Our team of
+          At CalorieTrack, we&apos;re passionate about empowering you to achieve
+          your health and fitness goals through mindful nutrition. Our team of
           experts has developed this web app with a mission to make calorie
           tracking simple, accurate, and personalized. With years of experience
-          in nutrition and technology, we've crafted CalorieTrack to be your
-          trusted companion on your wellness journey. We believe that
+          in nutrition and technology, we&apos;ve crafted CalorieTrack to be
+          your trusted companion on your wellness journey. We believe that
           understanding your food choices is crucial to a healthier lifestyle.
           Our commitment to excellence drives us to maintain an extensive food
           database, constantly updating it to ensure accuracy and inclusivity.
           We value your privacy and data security, so you can use our app with
-          confidence. Whether you're a fitness enthusiast, a health-conscious
-          individual, or someone looking to make lasting dietary changes,
-          CalorieTrack is here for you. Join us today, and together, let's
-          embark on a healthier, happier life.
+          confidence. Whether you&apos;re a fitness enthusiast, a
+          health-conscious individual, or someone looking to make lasting
+          dietary changes, CalorieTrack is here for you. Join us today, and
+          together, let&apos;s embark on a healthier, happier life.
         </p>
       </div>
     </section>

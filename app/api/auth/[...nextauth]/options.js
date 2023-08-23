d7e62e704997f56cfbede06a911c6c6973e1c9d4
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-08-23 12:35:00 +0300

    change basePath NextAuthProvider.jsx

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -50,7 +50,7 @@ export const options = {
       },
     }),
   ],
-  secret: "IuMYLp/T+nGzqd0Frff6Ho7+mEglpRmypHwSQn5KSgg=",
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/account/login",
   },
--- providers/NextAuthProvider.jsx
+++ providers/NextAuthProvider.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 const NextAuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath="calories-chi.vercel.app/api/auth">
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default NextAuthProvider;

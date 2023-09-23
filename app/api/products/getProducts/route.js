e4c849f6b0334d91466335574b9aff84658dcc64
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-09-23 23:23:28 +0300

    fix problem with date

--- app/api/products/getProducts/route.js
+++ app/api/products/getProducts/route.js
@@ -2,10 +2,12 @@ import { prisma } from "@/server/client/client";
 import { options } from "../../auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { zonedTimeToUtc } from "date-fns-tz";
 
 export async function POST(req) {
   const { date } = await req.json();
-  console.log("GET PRODUCT DATE:", { date });
+  const timeZone = "Atlantic/Azores";
+  const utcDate = zonedTimeToUtc(date, timeZone);
 
   const session = await getServerSession(options);
 
@@ -15,7 +17,7 @@ export async function POST(req) {
   const res = await prisma.diary.findMany({
     where: {
       userId: +id,
-      date,
+      date: utcDate,
     },
   });
 
